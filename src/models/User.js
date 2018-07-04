4b44a7e36001a134ecbcfd0b9b60ada37dc79e95
Amit Reif <amitreif@gmail.com>
2018-07-04 10:26:43 +0300

    everything up to topNavigation

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,6 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import uniqueValidator from 'mongoose-unique-validator';
 
-//TO DO: add uniqueness and email validation to email field
 const schema = new mongoose.Schema(
 {
   email: { type: String, required: true, lowercase: true, index: true, unique: true },
@@ -30,6 +29,10 @@ schema.methods.generateConfirmationUrl = function generateConfirmationUrl(){
   return `${process.env.HOST}/confirmation/${this.confirmationToken}`
 };
 
+schema.methods.generateResetPasswordLink = function generateResetPasswordLink(){
+  return `${process.env.HOST}/reset_password/${this.generateResetPasswordToken()}`
+};
+
 schema.methods.generateJWT = function generateJWT(){
   return jwt.sign(
     {
@@ -37,6 +40,16 @@ schema.methods.generateJWT = function generateJWT(){
     confirmed: this.confirmed
     },
     process.env.JWT_SECRET
+  );
+};
+
+schema.methods.generateResetPasswordToken = function generateResetPasswordToken(){
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h"}
   );
 };
 
